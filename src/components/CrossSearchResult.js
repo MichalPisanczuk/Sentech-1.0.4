2d8c9da23a587df908a4670d1f6010a8053c3736
Michał Pisańczuk <michal@pisanczuk.com>
2022-03-27 21:09:41 +0200

    Add search coresses

--- src/components/CrossSearchResult.js
+++ src/components/CrossSearchResult.js
@@ -6,56 +6,7 @@ import "../components/SearchResults.css";
 
 const crossApiUrl = "https://api-sentech.herokuapp.com/crosses";
 
-let SENTECH,
-  AC_Delco,
-  AD,
-  BERU,
-  BERU_K,
-  BOSCH,
-  BOSCH_K,
-  BOUGICORD,
-  BRECAV,
-  BREMI,
-  CAI,
-  CARHOFF_K,
-  CAROL,
-  CHAMPION,
-  DELPHI,
-  FACET,
-  FAE,
-  GEPARD,
-  GM,
-  HANS_PRISE,
-  HART,
-  HOLA,
-  JAPANPARTS,
-  JANMOR,
-  KAGER,
-  KERR_NELSON,
-  KRAFT,
-  LEADER,
-  LUCAS,
-  LYNX,
-  MASTER_SPORT,
-  MAXGEAR,
-  MEDBRYT,
-  MM_MSQ,
-  MM_MSQ_K,
-  MM_MSK,
-  MM_MSK_K,
-  MOPAR,
-  MOTORCRAFT,
-  NGK,
-  NGK_Stock_no,
-  NIPPARTS,
-  POWERTEC,
-  PROSPARK,
-  PROTECHNIC,
-  QH,
-  TESLA,
-  TOPRAN,
-  TUTELA,
-  OEM = "";
+let crossQueryExist = false;
 
 function CrossSearchResult(props) {
   const { t } = useTranslation();
@@ -78,219 +29,179 @@ function CrossSearchResult(props) {
       });
   }
 
-  if (props.crossPartId != "") {
-    crossesArray.map((item, index) => {
-      if (JSON.stringify(item).includes(props.crossPartId)) {
-        //return console.log(JSON.stringify(item), index);
-        SENTECH = item.SENTECH;
-        AC_Delco = item.AC_Delco;
-        AD = item.AD;
-        BERU = item.BERU;
-        BERU_K = item.BERU_K;
-        BOSCH = item.BOSCH;
-        BOSCH_K = item.BOSCH_K;
-        BOUGICORD = item.BOUGICORD;
-        BRECAV = item.BRECAV;
-        BREMI = item.BREMI;
-        CAI = item.CAI;
-        CARHOFF_K = item.CARHOFF_K;
-        CAROL = item.CAROL;
-        CHAMPION = item.CHAMPION;
-        DELPHI = item.DELPHI;
-        FACET = item.FACET;
-        FAE = item.FAE;
-        GEPARD = item.GEPARD;
-        GM = item.GM;
-        HANS_PRISE = item.HANS_PRISE;
-        HART = item.HART;
-        HOLA = item.HOLA;
-        JAPANPARTS = item.JAPANPARTS;
-        JANMOR = item.JANMOR;
-        KAGER = item.KAGER;
-        KERR_NELSON = item.KERR_NELSON;
-        KRAFT = item.KRAFT;
-        LEADER = item.LEADER;
-        LUCAS = item.LUCAS;
-        LYNX = item.LYNX;
-        MASTER_SPORT = item.MASTER_SPORT;
-        MAXGEAR = item.MAXGEAR;
-        MEDBRYT = item.MEDBRYT;
-        MM_MSQ = item.MM_MSQ;
-        MM_MSQ_K = item.MM_MSQ_K;
-        MM_MSK = item.MM_MSK;
-        MM_MSK_K = item.MM_MSK_K;
-        MOPAR = item.MOPAR;
-        MOTORCRAFT = item.MOTORCRAFT;
-        NGK = item.NGK;
-        NGK_Stock_no = item.NGK_Stock_no;
-        NIPPARTS = item.NIPPARTS;
-        POWERTEC = item.POWERTEC;
-        PROSPARK = item.PROSPARK;
-        PROTECHNIC = item.PROTECHNIC;
-        QH = item.QH;
-        TESLA = item.TESLA;
-        TOPRAN = item.TOPRAN;
-        TUTELA = item.TUTELA;
-        OEM = item.OEM;
-      }
-    });
+  if (props.crossPartId !== "") {
+    crossQueryExist = true;
+  } else {
+    crossQueryExist = false;
   }
 
   return (
     <div>
-      <h4>{t("Search results")}</h4>
-      <div>
-        <span>
-          <b> Sentech:</b> {SENTECH ? SENTECH : "No data"}
-        </span>
-        <span>
-          <b> AC Delco:</b> {AC_Delco ? AC_Delco : "No data"}
-        </span>
-        <span>
-          <b> AC:</b> {AD ? AD : "No data"}
-        </span>
-        <span>
-          <b> BERU:</b> {BERU ? BERU : "No data"}
-        </span>
-        <span>
-          <b> BERU (K):</b> {BERU_K ? BERU_K : "No data"}
-        </span>
-        <span>
-          <b> BOSCH:</b> {BOSCH ? BOSCH : "No data"}
-        </span>
-        <span>
-          <b> BOSCH (K):</b> {BOSCH_K ? BOSCH_K : "No data"}
-        </span>
-        <span>
-          <b> BOUGICORD:</b> {BOUGICORD ? BOUGICORD : "No data"}
-        </span>
-        <span>
-          <b> BRECAV:</b> {BRECAV ? BRECAV : "No data"}
-        </span>
-        <span>
-          <b> BREMI:</b> {BREMI ? BREMI : "No data"}
-        </span>
-        <span>
-          <b> CAI:</b> {CAI ? CAI : "No data"}
-        </span>
-        <span>
-          <b> CARHOFF (K):</b> {CARHOFF_K ? CARHOFF_K : "No data"}
-        </span>
-        <span>
-          <b> CAROL:</b> {CAROL ? CAROL : "No data"}
-        </span>
-        <span>
-          <b> CHAMPION:</b> {CHAMPION ? CHAMPION : "No data"}
-        </span>
-        <span>
-          <b> DELPHI:</b> {DELPHI ? DELPHI : "No data"}
-        </span>
-        <span>
-          <b> FACET:</b> {FACET ? FACET : "No data"}
-        </span>
-        <span>
-          <b> FAE:</b> {FAE ? FAE : "No data"}
-        </span>
-        <span>
-          <b> GEPARD:</b> {GEPARD ? GEPARD : "No data"}
-        </span>
-        <span>
-          <b> GM:</b> {GM ? GM : "No data"}
-        </span>
-        <span>
-          <b> HANS PRISE:</b> {HANS_PRISE ? HANS_PRISE : "No data"}
-        </span>
-        <span>
-          <b> HART:</b> {HART ? HART : "No data"}
-        </span>
-        <span>
-          <b> HOLA:</b> {HOLA ? HOLA : "No data"}
-        </span>
-        <span>
-          <b> JAPANPARTS:</b> {JAPANPARTS ? JAPANPARTS : "No data"}
-        </span>
-        <span>
-          <b> JANMOR:</b> {JANMOR ? JANMOR : "No data"}
-        </span>
-        <span>
-          <b> KAGER:</b> {KAGER ? KAGER : "No data"}
-        </span>
-        <span>
-          <b> KERR NELSON:</b> {KERR_NELSON ? KERR_NELSON : "No data"}
-        </span>
-        <span>
-          <b> KRAFT:</b> {KRAFT ? KRAFT : "No data"}
-        </span>
-        <span>
-          <b> LEADER:</b> {LEADER ? LEADER : "No data"}
-        </span>
-        <span>
-          <b> LUCAS:</b> {LUCAS ? LUCAS : "No data"}
-        </span>
-        <span>
-          <b> LYNX:</b> {LYNX ? LYNX : "No data"}
-        </span>
-        <span>
-          <b> MASTER SPORT:</b> {MASTER_SPORT ? MASTER_SPORT : "No data"}
-        </span>
-        <span>
-          <b> MAXGEAR:</b> {MAXGEAR ? MAXGEAR : "No data"}
-        </span>
-        <span>
-          <b> MEDBRYT:</b> {MEDBRYT ? MEDBRYT : "No data"}
-        </span>
-        <span>
-          <b> MM MSQ:</b> {MM_MSQ ? MM_MSQ : "No data"}
-        </span>
-        <span>
-          <b> MM MSQ (K):</b> {MM_MSQ_K ? MM_MSQ_K : "No data"}
-        </span>
-        <span>
-          <b> MM MSK:</b> {MM_MSK ? MM_MSK : "No data"}
-        </span>
-        <span>
-          <b> MM MSK (K):</b> {MM_MSK_K ? MM_MSK_K : "No data"}
-        </span>
-        <span>
-          <b> MOPAR:</b> {MOPAR ? MOPAR : "No data"}
-        </span>
-        <span>
-          <b> MOTORCRAFT:</b> {MOTORCRAFT ? MOTORCRAFT : "No data"}
-        </span>
-        <span>
-          <b> NGK:</b> {NGK ? NGK : "No data"}
-        </span>
-        <span>
-          <b> NGK Stock no:</b> {NGK_Stock_no ? NGK_Stock_no : "No data"}
-        </span>
-        <span>
-          <b> NIPPARTS:</b> {NIPPARTS ? NIPPARTS : "No data"}
-        </span>
-        <span>
-          <b> POWERTEC:</b> {POWERTEC ? POWERTEC : "No data"}
-        </span>
-        <span>
-          <b> PROSPARK:</b> {PROSPARK ? PROSPARK : "No data"}
-        </span>
-        <span>
-          <b> PROTECHNIC:</b> {PROTECHNIC ? PROTECHNIC : "No data"}
-        </span>
-        <span>
-          <b> QH:</b> {QH ? QH : "No data"}
-        </span>
-        <span>
-          <b> TESLA:</b> {TESLA ? TESLA : "No data"}
-        </span>
-        <span>
-          <b> TOPRAN:</b> {TOPRAN ? TOPRAN : "No data"}
-        </span>
-        <span>
-          <b> TUTELA:</b> {TUTELA ? TUTELA : "No data"}
-        </span>
-        <span>
-          <b> OEM:</b> {OEM ? OEM : "No data"}
-        </span>
-      </div>
+      <h4>{crossQueryExist ? t("Search results") : ""}</h4>
+      {crossQueryExist
+        ? crossesArray.map((item, index) => {
+            console.log(props.crossPartId);
+            if (JSON.stringify(item).includes(props.crossPartId)) {
+              return (
+                <div key={index}>
+                  <span>
+                    <b> Sentech:</b>{" "}
+                    <Link to={`/details/wires/${item.SENTECH}/${item.Marka}/${item.Typ_model}/${item.Pojemnosc_silnik}`}>
+                      {item.SENTECH ? <span className={item.SENTECH ? "result-link" : ""}>{item.SENTECH}</span> : "No data"}
+                    </Link>
+                  </span>
+                  <span>
+                    <b> AC Delco:</b> {item.AC_Delco ? item.AC_Delco : "No data"}
+                  </span>
+                  <span>
+                    <b> AC:</b> {item.AD ? item.AD : "No data"}
+                  </span>
+                  <span>
+                    <b> BERU:</b> {item.BERU ? item.BERU : "No data"}
+                  </span>
+                  <span>
+                    <b> BERU (K):</b> {item.BERU_K ? item.BERU_K : "No data"}
+                  </span>
+                  <span>
+                    <b> BOSCH:</b> {item.BOSCH ? item.BOSCH : "No data"}
+                  </span>
+                  <span>
+                    <b> BOSCH (K):</b> {item.BOSCH_K ? item.BOSCH_K : "No data"}
+                  </span>
+                  <span>
+                    <b> BOUGICORD:</b> {item.BOUGICORD ? item.BOUGICORD : "No data"}
+                  </span>
+                  <span>
+                    <b> BRECAV:</b> {item.BRECAV ? item.BRECAV : "No data"}
+                  </span>
+                  <span>
+                    <b> BREMI:</b> {item.BREMI ? item.BREMI : "No data"}
+                  </span>
+                  <span>
+                    <b> CAI:</b> {item.CAI ? item.CAI : "No data"}
+                  </span>
+                  <span>
+                    <b> CARHOFF (K):</b> {item.CARHOFF_K ? item.CARHOFF_K : "No data"}
+                  </span>
+                  <span>
+                    <b> CAROL:</b> {item.CAROL ? item.CAROL : "No data"}
+                  </span>
+                  <span>
+                    <b> CHAMPION:</b> {item.CHAMPION ? item.CHAMPION : "No data"}
+                  </span>
+                  <span>
+                    <b> DELPHI:</b> {item.DELPHI ? item.DELPHI : "No data"}
+                  </span>
+                  <span>
+                    <b> FACET:</b> {item.FACET ? item.FACET : "No data"}
+                  </span>
+                  <span>
+                    <b> FAE:</b> {item.FAE ? item.FAE : "No data"}
+                  </span>
+                  <span>
+                    <b> GEPARD:</b> {item.GEPARD ? item.GEPARD : "No data"}
+                  </span>
+                  <span>
+                    <b> GM:</b> {item.GM ? item.GM : "No data"}
+                  </span>
+                  <span>
+                    <b> HANS PRISE:</b> {item.HANS_PRISE ? item.HANS_PRISE : "No data"}
+                  </span>
+                  <span>
+                    <b> HART:</b> {item.HART ? item.HART : "No data"}
+                  </span>
+                  <span>
+                    <b> HOLA:</b> {item.HOLA ? item.HOLA : "No data"}
+                  </span>
+                  <span>
+                    <b> JAPANPARTS:</b> {item.JAPANPARTS ? item.JAPANPARTS : "No data"}
+                  </span>
+                  <span>
+                    <b> JANMOR:</b> {item.JANMOR ? item.JANMOR : "No data"}
+                  </span>
+                  <span>
+                    <b> KAGER:</b> {item.KAGER ? item.KAGER : "No data"}
+                  </span>
+                  <span>
+                    <b> KERR NELSON:</b> {item.KERR_NELSON ? item.KERR_NELSON : "No data"}
+                  </span>
+                  <span>
+                    <b> KRAFT:</b> {item.KRAFT ? item.KRAFT : "No data"}
+                  </span>
+                  <span>
+                    <b> LEADER:</b> {item.LEADER ? item.LEADER : "No data"}
+                  </span>
+                  <span>
+                    <b> LUCAS:</b> {item.LUCAS ? item.LUCAS : "No data"}
+                  </span>
+                  <span>
+                    <b> LYNX:</b> {item.LYNX ? item.LYNX : "No data"}
+                  </span>
+                  <span>
+                    <b> MASTER SPORT:</b> {item.MASTER_SPORT ? item.MASTER_SPORT : "No data"}
+                  </span>
+                  <span>
+                    <b> MAXGEAR:</b> {item.MAXGEAR ? item.MAXGEAR : "No data"}
+                  </span>
+                  <span>
+                    <b> MEDBRYT:</b> {item.MEDBRYT ? item.MEDBRYT : "No data"}
+                  </span>
+                  <span>
+                    <b> MM MSQ:</b> {item.MM_MSQ ? item.MM_MSQ : "No data"}
+                  </span>
+                  <span>
+                    <b> MM MSQ (K):</b> {item.MM_MSQ_K ? item.MM_MSQ_K : "No data"}
+                  </span>
+                  <span>
+                    <b> MM MSK:</b> {item.MM_MSK ? item.MM_MSK : "No data"}
+                  </span>
+                  <span>
+                    <b> MM MSK (K):</b> {item.MM_MSK_K ? item.MM_MSK_K : "No data"}
+                  </span>
+                  <span>
+                    <b> MOPAR:</b> {item.MOPAR ? item.MOPAR : "No data"}
+                  </span>
+                  <span>
+                    <b> MOTORCRAFT:</b> {item.MOTORCRAFT ? item.MOTORCRAFT : "No data"}
+                  </span>
+                  <span>
+                    <b> NGK:</b> {item.NGK ? item.NGK : "No data"}
+                  </span>
+                  <span>
+                    <b> NGK Stock no:</b> {item.NGK_Stock_no ? item.NGK_Stock_no : "No data"}
+                  </span>
+                  <span>
+                    <b> NIPPARTS:</b> {item.NIPPARTS ? item.NIPPARTS : "No data"}
+                  </span>
+                  <span>
+                    <b> POWERTEC:</b> {item.POWERTEC ? item.POWERTEC : "No data"}
+                  </span>
+                  <span>
+                    <b> PROSPARK:</b> {item.PROSPARK ? item.PROSPARK : "No data"}
+                  </span>
+                  <span>
+                    <b> PROTECHNIC:</b> {item.PROTECHNIC ? item.PROTECHNIC : "No data"}
+                  </span>
+                  <span>
+                    <b> QH:</b> {item.QH ? item.QH : "No data"}
+                  </span>
+                  <span>
+                    <b> TESLA:</b> {item.TESLA ? item.TESLA : "No data"}
+                  </span>
+                  <span>
+                    <b> TOPRAN:</b> {item.TOPRAN ? item.TOPRAN : "No data"}
+                  </span>
+                  <span>
+                    <b> TUTELA:</b> {item.TUTELA ? item.TUTELA : "No data"}
+                  </span>
+                  <span>
+                    <b> OEM:</b> {item.OEM ? item.OEM : "No data"}
+                  </span>
+                </div>
+              );
+            }
+          })
+        : ""}
     </div>
   );
 }
